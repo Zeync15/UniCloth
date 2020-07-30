50bed893c99fec3a282efeb18f526be92d78da81
Zeync15 <zeyenchai@gmail.com>
2020-07-30 12:26:23 +0800

    modify database

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ var express = require("express"),
     mongoose = require("mongoose"),
     passport = require("passport"),
     flash = require("connect-flash"),
-    session = require("express-session"),
-    User = require("./models/user");
+    session = require("express-session");
 
 // localhost database
 // mongoose.connect("mongodb://localhost/UniCloth", {
@@ -41,8 +40,6 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
 
 
 //Connect Flash
